58247886fd39214c64bad3c8e3384a0ea1038e28
Stepan Henek <stepan@henek.name>
2021-07-20 23:30:24 +0200

    feature: added event handling

--- pabitell-lib/src/lib.rs
+++ pabitell-lib/src/lib.rs
@@ -2,35 +2,43 @@ use anyhow::Result;
 use std::fmt;
 use uuid;
 
-pub trait Text {
-    fn plain(&self) -> &str;
-}
-
 pub trait Id {
     fn id(&self) -> &uuid::Uuid;
 }
 
-pub trait Item: Id + fmt::Debug {
+pub trait Description {
+    fn detail(&self, world: &Box<dyn World>) -> &str;
+    fn short(&self, world: &Box<dyn World>) -> &str;
+}
+
+pub trait Item: Id + Description + fmt::Debug {
     fn quantity(&self) -> Option<usize>;
 }
 
-pub trait Character: Id + fmt::Debug {
+pub trait Character: Id + Description + fmt::Debug {
+    fn items(&self) -> &[usize];
+    fn location(&self) -> Option<usize>;
+}
+
+pub trait Location: Id + Description + fmt::Debug {
     fn items(&self) -> &[usize];
 }
 
-pub trait Location: Id + fmt::Debug {
-    fn characters(&self) -> &[usize];
-    fn items(&self) -> &[usize];
+pub trait Event: Id + fmt::Debug {
+    fn characters(&self) -> Vec<usize> {
+        // Characters related to event
+        vec![]
+    }
+    fn can_be_triggered(&self, world: &Box<dyn World>) -> bool;
+    fn trigger(self, world: &mut Box<dyn World>) -> Option<Box<dyn Description>>;
 }
 
-pub trait StoryBuilder<S>
+pub trait WorldBuilder<S>
 where
-    S: Story,
+    S: World,
 {
     fn id(self, id: uuid::Uuid) -> Self;
-    fn intro(self, text: Box<dyn Text>) -> Self;
-    fn outro(self, text: Box<dyn Text>) -> Self;
-    fn description(self, text: Box<dyn Text>) -> Self;
+    fn description(self, text: Box<dyn Description>) -> Self;
     fn character(self, character: Box<dyn Character>) -> Self;
     fn item(self, item: Box<dyn Item>) -> Self;
     fn location(self, item: Box<dyn Location>) -> Self;
@@ -38,8 +46,8 @@ where
     fn build(self) -> Result<S>;
 }
 
-pub trait Story: Id {
-    fn description(&self) -> &Box<dyn Text>;
+pub trait World: Id {
+    fn description(&self) -> &Box<dyn Description>;
     fn locations(&self) -> &[Box<dyn Location>];
     fn characters(&self) -> &[Box<dyn Character>];
     fn items(&self) -> &[Box<dyn Item>];
@@ -47,7 +55,7 @@ pub trait Story: Id {
 
 #[cfg(test)]
 pub mod test {
-    use super::{Character, Id, Item, Location, Story, StoryBuilder, Text};
+    use super::{Character, Description, Event, Id, Item, Location, World, WorldBuilder};
     use anyhow::{anyhow, Result};
     use uuid;
 
@@ -55,6 +63,7 @@ pub mod test {
     struct TestCharacter {
         id: uuid::Uuid,
         items: Vec<usize>,
+        location: Option<usize>,
     }
 
     impl Id for TestCharacter {
@@ -63,9 +72,21 @@ pub mod test {
         }
     }
 
+    impl Description for TestCharacter {
+        fn short(&self, _: &Box<dyn World>) -> &str {
+            "Test Character"
+        }
+        fn detail(&self, _: &Box<dyn World>) -> &str {
+            "Character description and perhaps items which it is carying"
+        }
+    }
+
     impl Character for TestCharacter {
         fn items(&self) -> &[usize] {
             &self.items
+        }
+        fn location(&self) -> Option<usize> {
+            self.location
         }
     }
 
@@ -81,6 +102,15 @@ pub mod test {
         }
     }
 
+    impl Description for TestItem {
+        fn short(&self, _: &Box<dyn World>) -> &str {
+            "Test Location"
+        }
+        fn detail(&self, _: &Box<dyn World>) -> &str {
+            "Test location detailed description"
+        }
+    }
+
     impl Item for TestItem {
         fn quantity(&self) -> Option<usize> {
             Some(1)
@@ -90,7 +120,6 @@ pub mod test {
     #[derive(Debug, Default)]
     struct TestLocation {
         id: uuid::Uuid,
-        characters: Vec<usize>,
         items: Vec<usize>,
     }
 
@@ -100,28 +129,67 @@ pub mod test {
         }
     }
 
-    impl Location for TestLocation {
-        fn characters(&self) -> &[usize] {
-            &self.characters
+    impl Description for TestLocation {
+        fn short(&self, _: &Box<dyn World>) -> &str {
+            "Test Location"
         }
+        fn detail(&self, _: &Box<dyn World>) -> &str {
+            "Test location detailed description"
+        }
+    }
 
+    impl Location for TestLocation {
         fn items(&self) -> &[usize] {
             &self.items
         }
     }
 
-    struct TestText {
-        text: String,
+    #[derive(Clone, Debug)]
+    struct TestDescription;
+    impl Description for TestDescription {
+        fn short(&self, _: &Box<dyn World>) -> &str {
+            "Test Event"
+        }
+        fn detail(&self, _: &Box<dyn World>) -> &str {
+            "Test event to do something"
+        }
     }
-    impl Text for TestText {
-        fn plain(&self) -> &str {
-            &self.text
+
+    #[derive(Debug, Clone)]
+    struct TestEvent {
+        id: uuid::Uuid,
+        description: TestDescription,
+        success: TestDescription,
+        failure: TestDescription,
+    }
+    impl Id for TestEvent {
+        fn id(&self) -> &uuid::Uuid {
+            &self.id
+        }
+    }
+    impl Event for TestEvent {
+        fn trigger(self, world: &mut Box<dyn World>) -> Option<Box<dyn Description>> {
+            if self.can_be_triggered(world) {
+                return Some(Box::new(TestDescription));
+            } else {
+                None
+            }
+        }
+
+        fn can_be_triggered(&self, world: &Box<dyn World>) -> bool {
+            if let Some(location) = world.characters()[0].location() {
+                if location == 0 {
+                    return true;
+                }
+            }
+
+            false
         }
     }
 
     struct TestStory {
         id: uuid::Uuid,
-        description: Box<dyn Text>,
+        description: Box<dyn Description>,
         items: Vec<Box<dyn Item>>,
         locations: Vec<Box<dyn Location>>,
         characters: Vec<Box<dyn Character>>,
@@ -133,8 +201,8 @@ pub mod test {
         }
     }
 
-    impl Story for TestStory {
-        fn description(&self) -> &Box<dyn Text> {
+    impl World for TestStory {
+        fn description(&self) -> &Box<dyn Description> {
             &self.description
         }
 
@@ -154,31 +222,19 @@ pub mod test {
     #[derive(Default)]
     struct TestStoryBuilder {
         id: Option<uuid::Uuid>,
-        intro: Option<Box<dyn Text>>,
-        outro: Option<Box<dyn Text>>,
-        description: Option<Box<dyn Text>>,
+        description: Option<Box<dyn Description>>,
         characters: Vec<Box<dyn Character>>,
         items: Vec<Box<dyn Item>>,
         locations: Vec<Box<dyn Location>>,
     }
 
-    impl StoryBuilder<TestStory> for TestStoryBuilder {
+    impl WorldBuilder<TestStory> for TestStoryBuilder {
         fn id(mut self, id: uuid::Uuid) -> Self {
             self.id = Some(id);
             self
         }
 
-        fn intro(mut self, text: Box<dyn Text>) -> Self {
-            self.intro = Some(text);
-            self
-        }
-
-        fn outro(mut self, text: Box<dyn Text>) -> Self {
-            self.outro = Some(text);
-            self
-        }
-
-        fn description(mut self, text: Box<dyn Text>) -> Self {
+        fn description(mut self, text: Box<dyn Description>) -> Self {
             self.description = Some(text);
             self
         }
@@ -219,15 +275,7 @@ pub mod test {
     fn linear() {
         let story = TestStoryBuilder::default()
             .id(uuid::Uuid::new_v4())
-            .intro(Box::new(TestText {
-                text: "Some intro".into(),
-            }))
-            .outro(Box::new(TestText {
-                text: "Some outro".into(),
-            }))
-            .description(Box::new(TestText {
-                text: "Some description".into(),
-            }))
+            .description(Box::new(TestDescription))
             .character(Box::new(TestCharacter::default()))
             .character(Box::new(TestCharacter::default()))
             .item(Box::new(TestItem::default()))
