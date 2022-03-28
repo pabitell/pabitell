5d8681fdc25527e4af15a72061c944371783a157
Stepan Henek <stepan@henek.name>
2022-03-28 22:52:30 +0200

    refactor(pabitell-cli): remove unused code from pabitell-cli web
    Now only WS endpoint remains.

--- pabitell-cli/src/main.rs
+++ pabitell-cli/src/main.rs
@@ -48,17 +48,8 @@ pub fn main() {
         .author(crate_authors!())
         .version(crate_version!())
         .about(crate_description!())
-        .arg(
-            Arg::new("db-path")
-                .short('P')
-                .long("db-path")
-                .value_name("PATH")
-                .takes_value(true)
-                .required(true)
-                .env("PABITELL_DB_PATH"),
-        )
         .subcommands(vec![
-            App::new("cli").arg(
+            App::new("cli").args(&[
                 Arg::new("default-lang")
                     .short('L')
                     .long("default-lang")
@@ -66,7 +57,14 @@ pub fn main() {
                     .takes_value(true)
                     .default_value("en-US")
                     .env("PABITELL_DEFAULT_LANG"),
-            ),
+                Arg::new("db-path")
+                    .short('P')
+                    .long("db-path")
+                    .value_name("PATH")
+                    .takes_value(true)
+                    .required(true)
+                    .env("PABITELL_DB_PATH"),
+            ]),
             App::new("web").arg(
                 Arg::new("port")
                     .env("PORT")
@@ -80,14 +78,14 @@ pub fn main() {
         ]);
 
     let matches = app.clone().get_matches();
-    let db_path = matches.value_of("db-path").unwrap();
     match matches.subcommand() {
         Some(("web", web_matches)) => {
             let port = web_matches.value_of("port").unwrap();
-            web::start_web_app(db_path, port).unwrap();
+            web::start_web_app(port).unwrap();
         }
         Some(("cli", cli_matches)) => {
             let default_lang = cli_matches.value_of("default-lang").unwrap();
+            let db_path = matches.value_of("db-path").unwrap();
             cli::start_cli_app(default_lang, db_path).unwrap();
         }
         _ => exit_on_parse_error(app),
--- pabitell-cli/src/web.rs
+++ pabitell-cli/src/web.rs
@@ -1,175 +1,32 @@
 use actix::*;
-use actix_web::{
-    error, get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
-};
+use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
-use anyhow::{anyhow, Result};
-use futures::StreamExt;
-use pabitell_lib::{data::EventData, protocol, Dumpable, Narrator, World};
-use serde::{Deserialize, Serialize};
 use sled::Db;
-use std::{
-    sync::{atomic::AtomicUsize, Arc},
-    time::Instant,
-};
-use tracing::{debug, info, Level};
+use std::time::Instant;
+use tracing::info;
 use tracing_actix_web::TracingLogger;
-use tracing_subscriber::{self, filter::LevelFilter, EnvFilter};
+use tracing_subscriber::{self, EnvFilter};
 use uuid::Uuid;
 
-#[cfg(feature = "with_doggie_and_kitie_cake")]
-use crate::make_story_doggie_and_kitie_cake;
-#[cfg(feature = "with_doggie_and_kitie_doll")]
-use crate::make_story_doggie_and_kitie_doll;
-use crate::{
-    backend,
-    websocket::{ClientMessage as WsClientMessage, WsConnection, WsManager},
-};
+use crate::websocket::{WsConnection, WsManager};
 
 const WORKERS: usize = 8;
-const MAX_SIZE: usize = 1024 * 1024;
-
-#[derive(Serialize, Deserialize)]
-struct NewWorld {
-    id: Uuid,
-}
-
-fn make_world(_namespace: &str, story: &str) -> Option<(Box<dyn World>, Box<dyn Narrator>)> {
-    match story {
-        #[cfg(feature = "with_doggie_and_kitie_cake")]
-        "doggie_and_kitie_cake" => make_story_doggie_and_kitie_cake(true).unwrap(),
-        #[cfg(feature = "with_doggie_and_kitie_doll")]
-        "doggie_and_kitie_doll" => make_story_doggie_and_kitie_doll(true).unwrap(),
-        _ => None,
-    }
-}
-
-#[post("/api/{namespace}/{story}/")]
-async fn create_world(
-    data: web::Data<(Db, Addr<WsManager>)>,
-    path: web::Path<(String, String)>,
-) -> impl Responder {
-    let (namespace, story) = path.into_inner();
-    if let Some((world, _)) = make_world(&namespace, &story) {
-        backend::store(&mut data.as_ref().0.clone(), &story, world.as_ref()).unwrap();
-        HttpResponse::Created().json(NewWorld {
-            id: world.id().clone(),
-        })
-    } else {
-        HttpResponse::NotFound().json(serde_json::json!({"msg": "story not found"}))
-    }
-}
-
-#[get("/api/{namespace}/{story}/{world}/")]
-async fn get_world(
-    data: web::Data<(Db, Addr<WsManager>)>,
-    path: web::Path<(String, String, Uuid)>,
-) -> impl Responder {
-    let (namespace, story, world_id) = path.into_inner();
-    if let Some((mut world, _)) = make_world(&namespace, &story) {
-        if backend::load(
-            &mut data.as_ref().0.clone(),
-            &story,
-            &world_id,
-            world.as_mut(),
-        )
-        .is_ok()
-        {
-            HttpResponse::Ok().json(world.dump())
-        } else {
-            HttpResponse::NotFound().json(serde_json::json!({
-                "msg": format!("world not found (id={})", world_id)
-            }))
-        }
-    } else {
-        HttpResponse::NotFound().json(serde_json::json!({"msg": "story not found"}))
-    }
-}
-
-#[post("/api/{namespace}/{story}/{world}/event/")]
-async fn event_world(
-    data: web::Data<(Db, Addr<WsManager>)>,
-    path: web::Path<(String, String, Uuid)>,
-    mut payload: web::Payload,
-) -> std::result::Result<HttpResponse, Error> {
-    let (namespace, story, world_id) = path.into_inner();
-
-    let (mut world, narrator) = make_world(&namespace, &story).ok_or(error::ErrorNotFound(
-        serde_json::json!({"msg": "story not found"}),
-    ))?;
-
-    if backend::load(
-        &mut data.as_ref().0.clone(),
-        &story,
-        &world_id,
-        world.as_mut(),
-    )
-    .is_err()
-    {
-        return Ok(HttpResponse::NotFound().json(serde_json::json!({
-            "msg": format!("world not found (id={})", world_id)
-        })));
-    }
-
-    // read payload
-    let mut body = web::BytesMut::new();
-    while let Some(chunk) = payload.next().await {
-        let chunk = chunk?;
-        if (body.len() + chunk.len()) > MAX_SIZE {
-            return Err(error::ErrorBadRequest("overflow"));
-        }
-        body.extend_from_slice(&chunk);
-    }
-
-    // Test whether the data are JSON
-    let value =
-        serde_json::from_slice(&body).map_err(|_| error::ErrorBadRequest("Expected JSON"))?;
-
-    // Test whether it can be parsed as event
-    let mut event = narrator
-        .parse_event(world.as_ref(), value)
-        .ok_or(error::ErrorBadRequest("Wrong event data"))?;
-
-    // Test whether event can be triggered
-    if event.can_be_triggered(world.as_ref()) {
-        event.trigger(world.as_mut());
-        backend::store(&mut data.as_ref().0.clone(), &story, world.as_ref()).unwrap();
-
-        let ws_manager = data.as_ref().1.clone();
-        debug!("Sending;dump={}", event.dump());
-        ws_manager.do_send(WsClientMessage {
-            world_id,
-            data: serde_json::to_string(&protocol::Message::Notification(
-                protocol::NotificationMessage::Event(protocol::EventNotification {
-                    event: event.dump(),
-                    event_count: world.event_count(),
-                }),
-            ))
-            .unwrap(),
-        });
-
-        // TODO think of some reasonable retval
-        Ok(HttpResponse::Ok().json(serde_json::json!({"event_count": world.event_count()})))
-    } else {
-        Err(error::ErrorBadRequest("Event can't be triggered"))
-    }
-}
 
 #[get("/ws/{namespace}/{story}/{world}/")]
 async fn ws_endpoint(
     req: HttpRequest,
-    data: web::Data<(Db, Addr<WsManager>)>,
+    data: web::Data<Addr<WsManager>>,
     path: web::Path<(String, String, Uuid)>,
     stream: web::Payload,
 ) -> std::result::Result<HttpResponse, Error> {
     ws::start(
-        WsConnection::new(Instant::now(), path.2, data.get_ref().1.clone()),
+        WsConnection::new(Instant::now(), path.2, data.get_ref().clone()),
         &req,
         stream,
     )
 }
 
-async fn start(db_path: &str, port: &str) -> anyhow::Result<()> {
+async fn start(port: &str) -> anyhow::Result<()> {
     // setting logging collector
     let _collector = tracing_subscriber::fmt()
         .with_env_filter(
@@ -179,18 +36,13 @@ async fn start(db_path: &str, port: &str) -> anyhow::Result<()> {
         .try_init();
     info!("Logging");
 
-    // init db connection
-    let db = sled::open(db_path)?;
     // Start chat server actor
     let ws_manager = WsManager::new().start();
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new((db.clone(), ws_manager.to_owned())))
+            .app_data(web::Data::new(ws_manager.to_owned()))
             .wrap(TracingLogger::default())
-            .service(create_world)
-            .service(get_world)
-            .service(event_world)
             .service(ws_endpoint)
     })
     .workers(WORKERS)
@@ -200,7 +52,7 @@ async fn start(db_path: &str, port: &str) -> anyhow::Result<()> {
     .map_err(|e| e.into())
 }
 
-pub fn start_web_app(db_path: &str, port: &str) -> anyhow::Result<()> {
+pub fn start_web_app(port: &str) -> anyhow::Result<()> {
     actix_web::rt::System::with_tokio_rt(|| {
         tokio::runtime::Builder::new_multi_thread()
             .enable_all()
@@ -209,5 +61,5 @@ pub fn start_web_app(db_path: &str, port: &str) -> anyhow::Result<()> {
             .build()
             .unwrap()
     })
-    .block_on(start(db_path, port))
+    .block_on(start(port))
 }
