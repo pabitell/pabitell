0286d30783707052a6db2862dae678a9ad316e6a
Stepan Henek <stepan@henek.name>
2022-09-03 23:26:47 +0200

    chore: pabitell-root moved to a separate repo

--- pabitell-root/README.md
+++
@@ -1,3 +0,0 @@
-# Pabitell root
-
-The main webapp which is used to display links to each individual webapps of the stories.
--- pabitell-root/src/components.rs
+++
@@ -1,7 +0,0 @@
-pub mod book;
-pub mod books;
-pub mod breadcrumb;
-pub mod footer;
-pub mod help;
-pub mod language;
-pub mod title;
